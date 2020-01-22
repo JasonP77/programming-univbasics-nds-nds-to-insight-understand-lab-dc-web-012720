8213320168441a6e50655be7cc5e06b73e76160f
Jin Hyuk Park <llvslr77@gmail.com>
2020-01-22 23:42:35 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -4,8 +4,12 @@ require 'directors_database'
 
 def pretty_print_nds(nds)
   # Change the code below to pretty print the nds with pp
-  nil
+  pp nds
 end
 
 def print_first_directors_movie_titles
+  directors_database
+  row = 0 
+  while row < nds.count do 
+    
 end
