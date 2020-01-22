de1ad03bb5dcbad21d0e23edffbf81ab2c0d3204
Jin Hyuk Park <llvslr77@gmail.com>
2020-01-22 23:47:33 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -1,7 +1,7 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 # Call the method directors_database to retrieve the NDS
-
+directors_database
 def pretty_print_nds(nds)
   # Change the code below to pretty print the nds with pp
   pp nds
@@ -9,4 +9,12 @@ end
 
 def print_first_directors_movie_titles
   directors_database
+  row = 0 
+  while row < nds.count do 
+    new = []
+    element = 0 
+    while element < nds[row].count do 
+      if nds[row][element][:dirname] == "Spielburg"
+        new << nds[row][element][]
+    
 end
